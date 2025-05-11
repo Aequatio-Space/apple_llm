1adfbeddb84dea5eaa8aaed4715343d2dafe2444
aequatio <charlieCEA@163.com>
2025-05-11 09:22:58 +0800

    Add tests to finetune result.

--- chat_with_model_lite.py
+++ chat_with_model_lite.py
@@ -6,13 +6,20 @@ from transformers import pipeline
 pipe = pipeline("text-generation", model="/Users/aequatio/147-大模型本地/sft_fine_tune", torch_dtype=torch.bfloat16, device_map="auto")
 
 # We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
+# messages = [
+#     {
+#         "role": "system",
+#         "content": "Summarize the following paragraph with a topic and a few keywords.",
+#     },
+#     {"role": "user", "content": "Introduction To Swift Programming : Swift Cheat Sheet - A comprehensive Guide on Classes, Protocols, Extensions, Enums, Initializers, and Deinitializers"},
+# ]
 messages = [
     {
         "role": "system",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        "content": "You will receive a series of number with a specific pattern. Your task is to print user's input.",
     },
-    {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
+    {"role": "user", "content": "16 26 72 104 152 172 184"},
 ]
 prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+outputs = pipe(prompt, max_new_tokens=512, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
 print(outputs[0]["generated_text"])
